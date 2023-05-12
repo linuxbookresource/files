abfe8fc4080b0f28278031d6a4759fd0c06db3fd
linuxbookresource <133398778+linuxbookresource@users.noreply.github.com>
2023-05-12 14:08:38 -0400

    Update lab23.py

--- Day23/lab23.py
+++ Day23/lab23.py
@@ -1,1 +1,13 @@
+# Checks if a number represents a leap year
 
+def isLeapYear(n):
+
+
+# Testing the function
+if __name__ == '__main__':
+    print("Enter a year:")
+    n = int(input())
+    if isLeapYear(n):
+        print("yes")
+    else:
+        print("no")        
